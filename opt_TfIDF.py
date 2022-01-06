5275e692a87f363c7c4d37f7403ca399fd058223
NoySal <84030629+NoySal@users.noreply.github.com>
2022-01-07 01:35:14 +0200

    bm25

--- opt_TfIDF.py
+++ opt_TfIDF.py
@@ -9,15 +9,15 @@ from HWTFIDFPIPE import get_posting_gen , IR_Tokenize
 from heapq import nlargest
 from collections import Counter
 
-def get_OPT_Tfidf(q_tokens , index , N=100):
+def get_OPT_Tfidf(q_tokens , index ,corpus_docs, N=100):
     """
     Tfidf Optimized function , recieves tokens as a list of tokens (with duplicates)
     :param q_tokens:  list of tokens , with duplicatse.
     :param index:  inverted index.
+    :param corpus_docs : int , optimization - number of docs in corpus
     :return: list of documents : title , sorted by their IDF score with query
     """
     q_size = len(q_tokens)
-    corpus_docs = len(index.DL)
     q_tokens = list(Counter(q_tokens).items())
     sim_q = {}
     for q_word , q_count in q_tokens:
@@ -39,3 +39,37 @@ def get_OPT_Tfidf(q_tokens , index , N=100):
         top_100 = nlargest(N, sim_q , key = sim_q.get )
         return sorted(top_100 ,reverse=True)
 
+def get_opt_BM25(q_tokens , index ,corpus_docs,avg_dl , k =1.5,b =0.75 , N=100):
+    """
+    BM25 retrival model from inverted index .
+    param q_tokens - list of str,  tokens of words processed like corpus.
+    param index : inverted index class
+    :param corpus_docs : int , optimization - number of docs in corpus
+    param avg_dl : float, average document size in corpus
+    param k : float, range between [1.2 ,2] optimized to corpus
+    param b : float , ~0.75 optimizable
+    param N : int , number of best-fit docs to retrive
+    :return: list of documents : title , sorted by their IDF score with query
+    """
+
+    q_size = len(q_tokens)
+    q_tokens = list(Counter(q_tokens).items())
+    sim_q = {}
+    for q_word , q_count in q_tokens:
+        q_idf =  np.log((1+corpus_docs) / (index.df[q_word] + 0.5 ))
+        if q_word in index.term_total.keys():
+            for doc_id, word_count in read_posting_list(index, q_word):
+                tw = word_count * (k+1) / ( word_count + k * (  1 - b + b * index.DL[doc_id] / avg_dl ))
+                if doc_id in sim_q.keys():
+                    sim_q[doc_id] += tw * q_idf
+                else:
+                    sim_q[doc_id] = tw * q_idf
+
+    for doc_id in sim_q.keys():
+        sim_q[doc_id] = sim_q[doc_id]*(1/q_size) * (1/index.DL[doc_id])
+
+    if len(sim_q) < 100:
+        return sorted(sim_q , key = sim_q.get ,reverse=True)
+    else: #use heap sort and return
+        top_100 = nlargest(N, sim_q , key = sim_q.get )
+        return sorted(top_100 ,reverse=True)
