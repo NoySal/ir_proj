6843849eafed94dc0b331c7c219734eebcf80307
NoySal <84030629+NoySal@users.noreply.github.com>
2022-01-08 22:44:51 +0200

    opt BM25 changed to a better model k1 = 3, b= 0.25

--- opt_TfIDF.py
+++ opt_TfIDF.py
@@ -41,7 +41,7 @@ def get_OPT_Tfidf(q_tokens , index ,corpus_docs, N=100):
         top_100 = nlargest(N, sim_q , key = sim_q.get )
         return sorted(top_100 ,reverse=True)
 
-def get_opt_BM25(q_tokens , index ,corpus_docs , avg_dl , k =1.5,b =0.75 , N=100):
+def get_BM25(q_tokens , index ,corpus_docs , avg_dl , k =1.5,b =0.75 , N=100):
     """
     BM25 retrival model from inverted index .
     param q_tokens - list of str,  tokens of words processed like corpus.
@@ -78,3 +78,40 @@ def get_opt_BM25(q_tokens , index ,corpus_docs , avg_dl , k =1.5,b =0.75 , N=100
     else: #use heap sort and return
         top_100 = nlargest(N, sim_q , key = sim_q.get )
         return sorted(top_100 ,reverse=True)
+
+    def get_opt_BM25(q_tokens, index, corpus_docs, avg_dl, k=3, b=0.25, N=100):
+        """
+        BM25 retrival model from inverted index .
+        param q_tokens - list of str,  tokens of words processed like corpus.
+        param index : inverted index class
+        :param corpus_docs : int , optimization - number of docs in corpus
+        param avg_dl : float, average document size in corpus
+        param k : float, range between [1.2 ,2] optimized to corpus
+        param b : float , ~0.75 optimizable
+        param N : int , number of best-fit docs to retrive
+        :return: list of documents : title , sorted by their IDF score with query
+        """
+        ####
+        # if this method is good , we can create one new posting list , and one new q_idf dictionary.
+        # or at least q_idf dictionary for all the words.
+        ####
+        q_tokens = list(Counter(q_tokens).items())
+        sim_q = {}
+        for q_word, q_count in q_tokens:
+            if q_word in index.term_total.keys():
+                q_idf = np.log((1 + corpus_docs) / (index.df[q_word] + 0.5))
+                if q_word in index.term_total.keys():
+                    for doc_id, word_count in read_posting_list(index, q_word):
+                        if doc_id == 0:  # friggin missing values!
+                            continue
+                        tw = word_count * (k + 1) / (word_count + k * (1 - b + b * index.DL[doc_id] / avg_dl))
+                        if doc_id in sim_q.keys():
+                            sim_q[doc_id] += tw * q_idf
+                        else:
+                            sim_q[doc_id] = tw * q_idf
+
+        if len(sim_q) < 100:
+            return sorted(sim_q, key=sim_q.get, reverse=True)
+        else:  # use heap sort and return
+            top_100 = nlargest(N, sim_q, key=sim_q.get)
+            return sorted(top_100, reverse=True)
